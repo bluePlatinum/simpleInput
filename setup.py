6b9834efd677a5bd6da8af9136c9b710668c34ab
bluePlatinum <33969983+bluePlatinum@users.noreply.github.com>
2020-01-08 21:26:12 +0100

    create setup.py (#16)

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='simpleInput',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='bluePlatinum',
+    author_email='jukic.rok@gmail.com',
+    description=''
+)

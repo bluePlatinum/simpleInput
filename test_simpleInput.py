4878345da74b19e04e9174cbda8366e9c7e52a05
bluePlatinum <33969983+bluePlatinum@users.noreply.github.com>
2020-01-08 19:49:50 +0100

    deleted constructor for TestClass (#5)

--- test_simpleInput.py
+++ test_simpleInput.py
@@ -2,9 +2,6 @@ import simpleInput
 
 
 class TestClass:
-    def __init__(self):
-        pass
-
     def test_integer(self):
         simpleInput.input = lambda: 5
         result = simpleInput.sinput("", __builtins__.int, "", 0, 10)

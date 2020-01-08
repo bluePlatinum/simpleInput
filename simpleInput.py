a9c5a3a8fd6d92b2c9c88399f2a1030ed46cbe90
bluePlatinum <33969983+bluePlatinum@users.noreply.github.com>
2020-01-08 19:27:29 +0100

    create simpleInput.py
    added sinput function, which acts as the main function for this module (#2)

---
+++ simpleInput.py
@@ -0,0 +1,24 @@
+"""Library for simplified input handling"""
+
+
+def sinput(prompt_text, input_type, failure_text, range_low, range_high):
+    """
+    simpleInput input handler
+    :param prompt_text: Text to show when prompting for input
+    :param input_type: Type of desired input
+    :param failure_text: Text to show if input is not accepted
+    :param range_low: Low range-end for input
+    :param range_high: High range-end
+    :return: input
+    """
+
+    while True:
+        given_input = input(prompt_text)
+        if type(given_input) == input_type:
+            if type(given_input) == __builtins__.int or type(given_input) == __builtins__.float_:
+                if range_low <= given_input and given_input <= range_high:
+                    return given_input
+            else:
+                return given_input
+        else:
+            print(failure_text) 

808c43a2c39b129a4e60fcb7c4a8f6a75d4377f5
bluePlatinum <33969983+bluePlatinum@users.noreply.github.com>
2020-01-08 21:34:35 +0100

    fixed problems with packages (#17)
    Signed-off-by: bluePlatinum <jukic.rok@gmail.com>

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='simpleInput',
     version='1.0',
-    packages=[''],
+    packages=find_packages(exclude=("configs", "tests")),
     url='',
     license='',
     author='bluePlatinum',

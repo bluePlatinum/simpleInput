14a83f82f65420b79667dac19ce78d757564dfc3
bluePlatinum <33969983+bluePlatinum@users.noreply.github.com>
2020-01-08 19:41:55 +0100

    Create float test (#4)
    * added test_float function to TestClass
    
    * reversed assert order so pytest will check for type first
    
    * added __init__() method to TestClass

--- test_simpleInput.py
+++ test_simpleInput.py
@@ -1,8 +1,18 @@
 import simpleInput
 
+
 class TestClass:
+    def __init__(self):
+        pass
 
     def test_integer(self):
         simpleInput.input = lambda: 5
         result = simpleInput.sinput("", __builtins__.int, "", 0, 10)
+        assert type(result) == __builtins__.int
         assert result == 5
+
+    def test_float(self):
+        simpleInput.input = lambda: 3.1
+        result = simpleInput.sinput("", __builtins__.float, "", 0, 5)
+        assert type(result) == __builtins__.float
+        assert result == 3.1
